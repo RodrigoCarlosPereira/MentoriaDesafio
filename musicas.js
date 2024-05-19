9dc98a757ed8780a18e88553fd510d77d5d8af54
Rodrigo Pereira <rodrigocarlospereira.16@outlook.com>
2024-05-19 17:44:08 -0300

    final

--- musicas.js
+++ musicas.js
@@ -1,17 +1,28 @@
 export default [
     {
-        src:"./src/We Are The World.mp3",
+        src:"src/MUSICAS/We Are The World.mp3",
         name: "We Are The World",
-        img: "./src/weImg.jpg",
+        img: "src/IMG/weImg.jpg",
     },
     {
-        src:"./src/Sunny.mp3",
+        src:"src/MUSICAS/Sunny.mp3",
         name:"Sunny",
-        img: "./src/fundo-musica-sunny.jpg",
+        img: "src/IMG/fundo-musica-sunny.jpg",
     },
     {
-        src:"./src/Welcome To The Jungle.mp3",
+        src:"src/MUSICAS/Welcome To The Jungle.mp3",
         name:"Welcome To The Jungle",
-        img: "./src/jungleImg.jpg",
+        img: "src/IMG/jungleImg.jpg",
+    },
+
+    {
+        src:"src/MUSICAS/Cego.mp3",
+        name:"Cego",
+        img:"src/IMG/cegoIMG.jpeg",
+    },
+    {
+        src:"src/MUSICAS/Everlong.mp3",
+        name:"Everlong",
+        img:"src/IMG/everIMG.jpeg"
     }
 ]
